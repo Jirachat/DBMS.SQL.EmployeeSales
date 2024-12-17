ee2751481f88e781e1dd9eb238a4bf1e43761cf5
Lenovo <Lenovo@LAPTOP-5N08LR59>
2024-12-17 15:12:50 +0700

    Add project files.

---
+++ DBMS.SQL.EmployeeSales/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace DBMS.SQL.EmployeeSales
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dgvEmployees = new DataGridView();
+            textBox1 = new TextBox();
+            dgvOrderList = new DataGridView();
+            textBox2 = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)dgvEmployees).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dgvOrderList).BeginInit();
+            SuspendLayout();
+            // 
+            // dgvEmployees
+            // 
+            dgvEmployees.AllowUserToAddRows = false;
+            dgvEmployees.AllowUserToDeleteRows = false;
+            dgvEmployees.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvEmployees.Location = new Point(12, 48);
+            dgvEmployees.Name = "dgvEmployees";
+            dgvEmployees.ReadOnly = true;
+            dgvEmployees.RowHeadersWidth = 51;
+            dgvEmployees.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvEmployees.Size = new Size(776, 167);
+            dgvEmployees.TabIndex = 0;
+            dgvEmployees.CellMouseUp += dgvEmployees_CellMouseUp_1;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 12);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 1;
+            textBox1.Text = "Employee";
+            // 
+            // dgvOrderList
+            // 
+            dgvOrderList.AllowUserToAddRows = false;
+            dgvOrderList.AllowUserToDeleteRows = false;
+            dgvOrderList.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvOrderList.Location = new Point(12, 291);
+            dgvOrderList.Name = "dgvOrderList";
+            dgvOrderList.ReadOnly = true;
+            dgvOrderList.RowHeadersWidth = 51;
+            dgvOrderList.Size = new Size(776, 147);
+            dgvOrderList.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(12, 258);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 3;
+            textBox2.Text = "OrderList";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox2);
+            Controls.Add(dgvOrderList);
+            Controls.Add(textBox1);
+            Controls.Add(dgvEmployees);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvEmployees).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dgvOrderList).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dgvEmployees;
+        private TextBox textBox1;
+        private DataGridView dgvOrderList;
+        private TextBox textBox2;
+    }
+}
---
+++ DBMS.SQL.EmployeeSales/Form1.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace DBMS.SQL.EmployeeSales
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //ประกาศตัวแปร connect
+        SqlConnection conn;
+        SqlCommand cmd;
+        SqlDataAdapter da;
+
+        private void connectDB()
+        {
+            string server = @"LAPTOP-5N08LR59\SQLEXPRESS";
+            string db = "Northwind";
+            string strCon = string.Format(@"Data Source={0}; Initial Catalog={1};"
+                      + "Integrated Security=True;Encrypt=False", server, db);
+            conn = new SqlConnection(strCon);
+            conn.Open();
+        }
+
+        private void disconnectDB()
+        {
+            conn.Close();
+        }
+
+        private void showdata(string sql, DataGridView dgv)
+        {
+            da = new SqlDataAdapter(sql, conn);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dgv.DataSource = ds.Tables[0];
+        }
+        private void dgvEmployees_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connectDB();
+            string sqlQuery = "select * from EmployeeList";
+            showdata(sqlQuery, dgvEmployees);
+        }
+
+
+        private void dgvEmployees_CellMouseUp_1(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.RowIndex != -1)
+            {
+                {
+                    int id = Convert.ToInt32(dgvEmployees.CurrentRow.Cells[0].Value);
+                    string sqlQuery = "select * from OrderList2 where EmployeeID = @id";
+                    cmd = new SqlCommand(sqlQuery, conn);
+                    cmd.Parameters.AddWithValue("@id", id);
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataSet ds = new DataSet();
+                    da.Fill(ds);
+                    dgvOrderList.DataSource = ds.Tables[0];
+                }
+            }
+        }
+    }
+}
